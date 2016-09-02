c63b8045d5adb3efc4edbfb1b5110f0a62cc5294
Chris Lintecum <clintecum2@gmail.com>
2016-09-02 13:01:37 -0500

    fix service

--- modules/places/client/services/places.client.service.js
+++ modules/places/client/services/places.client.service.js
@@ -38,13 +38,7 @@ angular.module('places.services').factory('findPlacesByZipService', function($ht
 
 angular.module('places.services').factory('getPlacesService', function($http) {
   var getPlaces = function(callback) {
-
-    var config = {
-      method: 'GET', 
-      url: 'api/places'
-    }
-
-    $http.jsonp('api/places').success(function(data) {
+    $http.get('/api/places').success(function(data) {
       callback(data);
     });
   };
